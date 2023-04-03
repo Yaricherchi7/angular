58c8b004a08878549bea61c167704ff63a5dd6e4
Cherchi Yari <Y.Cherchi@almaviva.it>
2023-04-03 14:35:39 +0200

    commit generale, aggiunta card e navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular';
+  restaurants : any = [
+    {
+      id: "92fb9513-73ad-5de9-a45c-769f33befcb8",
+      restaurantName: "Pots and Ladles",
+      companyName: "Zions Bancorporation",
+      cuisine: [
+        "British Virgin Islands"
+      ],
+      phone: "+48 91 707 98 15",
+      email: "badon@ZionsBancorporation.com",
+      geolocation: {
+        country: "Bermuda",
+        city: "Sioux Falls",
+        address: "63843 Leora Circles Apt. 112"
+      },
+      sideProducts: [
+        {
+          name: "Table",
+          material: "Wooden",
+          description: "New ABC 13 9370, 13.3, 5th Gen CoreA5-8250U, 8GB RAM, 256GB SSD, power UHD Graphics, OS 10 Home, OS Office A & J 2016",
+          adjectives: "Luxurious",
+          color: "purple"
+        }
+      ],
+      priceRange: [
+        [
+          "$83.00",
+          "$158"
+        ]
+      ]
+    }
+  ]
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { CardComponent } from './card/card.component';
+import {MatCardModule} from '@angular/material/card';
+import { NavBarComponent } from './nav-bar/nav-bar.component'
+import {MatToolbarModule} from '@angular/material/toolbar'
+import {MatIconModule} from '@angular/material/icon'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
